c9b2b1a204e59ac417d9570c1fb30380760c119d
Matheus Ednei Nepomuceno Souza <matheussouza@MacBook-Pro-de-Matheus.local>
2022-10-23 12:14:21 -0300

    add exercism folder and freelancer-rate exercise

---
+++ exercicios/exercism/freelancer-rates.js
@@ -0,0 +1,31 @@
+/**
+ * Exercises from exercism.org
+ *
+ * Freelancer Rates.
+ */
+
+const hourWorkedDay = 8;
+const billableDays = 22;
+
+dayRate = () => {
+  return hourlyRate * hourWorkedDay;
+};
+
+daysInBudget = (budget, hourlyRate) => {
+  return Math.floor(budget / dayRate(hourlyRate));
+};
+
+priceWithMonthlyDiscount = (hourlyRate, numberDays, monthlyDiscountRate) => {
+  const months = Math.floor(numberDays / billableDays);
+  const monthPrince = months * billableDays * dayRate(hourlyRate);
+  const remainingDays = numberDays % billableDays; 
+  return Math.ceil(monthPrince + remainingDays * dayRate(hourlyRate) - monthlyDiscountRate * monthPrince );
+};
+
+console.log(dayRate(89));
+console.log(daysInBudget(20000, 89));
+console.log(priceWithMonthlyDiscount(89, 230, 0.42));
+
+module.export = { dayRate, daysInBudget, priceWithMonthlyDiscount }
+
+
