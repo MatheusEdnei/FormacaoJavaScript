4d96a915f647f9eed9fb6e92b46a468b0e1fd529
Matheus Ednei <ednei880@gmail.com>
2022-10-25 14:42:32 -0300

    refactor poetry club

---
+++ exercicios/exercism/poetry-club.js
@@ -0,0 +1,25 @@
+/**
+ * Exercises from exercism.org
+ *
+ * Poetry Club Door Policy.
+ */
+
+frontDoorResponse = (line) => line.charAt(0);
+
+frontDoorPassword = (password) => capitalize(password);
+
+backDoorResponse = (line) => line.trim().charAt(line.trim().length - 1);
+
+backDoorPassword = (password) => `${capitalize(password)}, please`;
+
+
+capitalize = (word) => {
+  const lower = word.toLowerCase();
+  const firstLetter = word.charAt(0).toUpperCase();
+  return firstLetter + lower.slice(1);
+};
+
+console.log(frontDoorResponse("Stands"));
+console.log(frontDoorPassword("SHIRE"));
+console.log(backDoorResponse("Stands so high   "));
+console.log(backDoorPassword("horse"));
