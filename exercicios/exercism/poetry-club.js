919ec112c4086c125236a45c7e02585e9538ed14
Matheus Ednei <ednei880@gmail.com>
2022-10-25 19:44:44 -0300

    add package.json

--- exercicios/exercism/poetry-club.js
+++ exercicios/exercism/poetry-club.js
@@ -4,7 +4,10 @@
  * Poetry Club Door Policy.
  */
 
-frontDoorResponse = (line) => line.charAt(0);
+frontDoorResponse = (line) => {
+  console.log(line);
+  return line.charAt(0);
+};
 
 frontDoorPassword = (password) => capitalize(password);
 
@@ -12,14 +15,15 @@ backDoorResponse = (line) => line.trim().charAt(line.trim().length - 1);
 
 backDoorPassword = (password) => `${capitalize(password)}, please`;
 
-
 capitalize = (word) => {
   const lower = word.toLowerCase();
   const firstLetter = word.charAt(0).toUpperCase();
   return firstLetter + lower.slice(1);
 };
 
-console.log(frontDoorResponse("Stands"));
-console.log(frontDoorPassword("SHIRE"));
-console.log(backDoorResponse("Stands so high   "));
-console.log(backDoorPassword("horse"));
+// console.log(frontDoorResponse("Stands"));
+// console.log(frontDoorPassword("SHIRE"));
+// console.log(backDoorResponse("Stands so high   "));
+// console.log(backDoorPassword("horse"));
+
+module.export = frontDoorResponse();
