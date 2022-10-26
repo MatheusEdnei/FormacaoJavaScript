e19cdbf110c0bc5e5c7096dfef1e205edf05b815
Matheus Ednei <ednei880@gmail.com>
2022-10-26 14:12:37 -0300

    add treatment of error

--- criando-primeira-biblioteca/index.js
+++ criando-primeira-biblioteca/index.js
@@ -1,10 +1,18 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function treatsErro(erro) {
+    throw new Error(chalk.red(erro.code, ' did not find the file'));
+}
+
 // dont work with arrow function, why?
 function getFile(pathFile) {
     const encoding = 'utf-8';
-    fs.readFile(pathFile, encoding, (_, text) => {
+    fs.readFile(pathFile, encoding, (erro, text) => {
+        if (erro) {
+            treatsErro(erro);
+        }
+
         console.log(chalk.green(text));
     });
 }
