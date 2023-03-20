cce7e465220d32a38a1098c7ec03e90911a7588d
Matheus Ednei <ednei880@gmail.com>
2023-03-20 07:29:11 -0300

    adiciona metodos e atributos privados

--- oo/index.js
+++ oo/index.js
@@ -1,6 +1,6 @@
-import User from './User'
-import Docente from './Docente'
-import Admin from './Admin'
+import User from './User.js'
+import Docente from './Docente.js'
+import Admin from './Admin.js'
 
 const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01')
 console.log(novoAdmin.criaCurso('JS', 20))
@@ -10,7 +10,7 @@ const docente = new Docente ('Mariana', 'm@m.com', '2021-01-01')
 console.log(docente.aprovaEstudante('Jose', 'Historia'))
 
 const novoUser = new User('Maria', 'm@m.com', '2021-01-01')
-console.log(novoUser.exibirInfos());
+console.log(novoUser.exibirInfos())
 
 // novoUser.#nome = 'Marcia'
 // console.log(novoUser.nome);
