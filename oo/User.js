75cbdc25850ba33c02245791e6923234b5064fc9
Matheus Ednei <ednei880@gmail.com>
2023-03-20 08:05:17 -0300

    testes com getter e setter

--- oo/User.js
+++ oo/User.js
@@ -4,16 +4,23 @@ export default class User {
     #nascimento
     #role
     #ativo
-    constructor( nome, email, nascimento, role, ativo = true) {
+    #sobrenome
+
+    constructor( nome, sobrenome, email, nascimento, role, ativo = true) {
         this.#nome = nome
         this.#email = email
         this.#nascimento = nascimento
         this.#role = role || 'estudante'
         this.#ativo = ativo
+        this.#sobrenome = sobrenome
     }
 
     get nome() {
-        return this.#nome
+        return `${this.#nome} ${this.#sobrenome}`
+    }
+
+    get sobrenome() {
+        return this.#sobrenome
     }
 
     get email () {
@@ -32,6 +39,30 @@ export default class User {
         return this.#ativo
     }
 
+    set nome (nome) {
+        if (nome === '') throw new Error('formato inválido')
+        let [novoNome, ...sobrenome] = nome.split(" ")
+        sobrenome = sobrenome.join(' ')
+        this.#nome = novoNome
+        this.#sobrenome = sobrenome
+    }
+
+    set email (email) {
+        this.#email = email
+    }
+
+    set nascimento (nascimento) {
+        this.#nascimento = nascimento
+    }
+
+    set role (role) {
+        this.#role = role
+    }
+
+    set ativo (ativo) {
+        this.#ativo = ativo
+    }
+
     #montaObjUser() {
         return ({
             nome: this.#nome,
@@ -43,7 +74,6 @@ export default class User {
     }
 
     exibirInfos() {
-        const objUser = this.#montaObjUser()
-        return `${objUser.nome}, ${objUser.email}, ${objUser.nascimento},${objUser.role}, ${objUser.ativo}`
+        return `${this.nome}, ${this.email}, ${this.nascimento},${this.role}, ${this.ativo}`
     }
 }
--- oo/index.js
+++ oo/index.js
@@ -3,15 +3,23 @@ import Docente from './Docente.js'
 import Admin from './Admin.js'
 
 const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01')
-console.log(novoAdmin.criaCurso('JS', 20))
+// console.log(novoAdmin.criaCurso('JS', 20))
+console.log(novoAdmin.nome)
+
+novoAdmin.nome = 'André'
+
 console.log(novoAdmin.nome)
 
 
 const docente = new Docente ('Mariana', 'm@m.com', '2021-01-01')
-console.log(docente.aprovaEstudante('Jose', 'Historia'))
+// console.log(docente.aprovaEstudante('Jose', 'Historia'))
 
 const novoUser = new User('Maria', 'm@m.com', '2021-01-01')
-console.log(novoUser.exibirInfos())
+console.log(novoUser.nome)
+novoUser.nome = 'Juliana Silva Souza'
+console.log(novoUser.nome)
+console.log(novoUser.sobrenome)
+// console.log(novoUser.exibirInfos())
 
 // novoUser.#nome = 'Marcia'
 // console.log(novoUser.nome);
