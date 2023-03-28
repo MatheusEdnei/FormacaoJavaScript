32b7d32ff1709c43b7ce0dbf84ed6bd18baec765
Matheus Ednei <ednei880@gmail.com>
2023-03-28 08:04:49 -0300

    trabalhando com splice e lista com duas dimensões

---
+++ arrays/animais-marinhos.js
@@ -0,0 +1,7 @@
+const animaisDoAquario = ['Tubarão', 'Polvo', 'Golfinho', 'Golfinho cinza']
+
+animaisDoAquario.splice(1, 0, 'Peixe')
+console.log(animaisDoAquario)
+animaisDoAquario.splice(3, 2, 'Peixe azul')
+
+console.log(animaisDoAquario)
