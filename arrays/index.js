ba5b6fd124a406f3450cd6bff81b267854a0489b
Matheus Ednei <ednei880@gmail.com>
2023-03-28 10:51:31 -0300

    trabalhando com maps

--- arrays/index.js
+++ arrays/index.js
@@ -10,8 +10,18 @@
 // console.log(arrayConcat)
 // console.log(arrayOriginal)
 
-const arrayOriginal = [50, 60, 70]
-const arrayConcat = arrayOriginal.concat([80, [90, 100]])
+// const arrayOriginal = [50, 60, 70]
+// const arrayConcat = arrayOriginal.concat([80, [90, 100]])
 
-console.log(arrayConcat)
-console.log(arrayOriginal)
+// console.log(arrayConcat)
+// console.log(arrayOriginal)
+
+const arrayNums = [1, 2, 3, 4]
+
+function multiplicaPorDez(num) {
+    return num * 10
+}
+
+const arraySomada = arrayNums.map(multiplicaPorDez)
+
+console.log(arraySomada)
